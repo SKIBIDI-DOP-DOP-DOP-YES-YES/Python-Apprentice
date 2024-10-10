a1ab5a7c524ffbad53364191be046172acc5e2d3
SKIBIDI-DOP-DOP-DOP-YES-YES <yukari8327@hotmail.com>
2024-10-10 22:46:41 +0000

    knbdflliaeknhiglsjfbnlkxubdhjkvlskjbhupdsfugpfijphogfdnjpboepnbjpodwhnodfjgijofihufdopisgjipofjflgdkmd;lkgjs;kjfgiopjfdpwiohtewpri9u976709837460953487609385760593487609354867puoitpytoeituptoeiryuporiurepoiyueroipuyetoipetyuetuiyieyihoprgipriorhgjrgoihjjitdroieu-

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -9,3 +9,33 @@ write a new program that uses a loop to draw a pentagon.
 """
 
 ... # Your code here
+import turtle
+tina= turtle
+tina.speed (1)
+tina.pendown
+tina.begin_fill()
+tina.color('red')
+tina.forward(250)
+tina.left(90)
+tina.forward(250)
+tina.left(90)
+tina.forward(250)
+tina.left(90)
+tina.forward(250)
+tina.left(90)
+tina.forward(125)
+tina.end_fill()
+tina.fillcolor('red')
+tina.left(90)
+tina.forward(25)
+tina.right(90)
+
+tina.penup()
+tina.pendown()
+tina.begin_fill()
+tina.fillcolor('white')
+tina.circle(75)
+tina.forward(50)
+tina.left(45)
+tina.end_fill()
+turtle.exitonclick()
