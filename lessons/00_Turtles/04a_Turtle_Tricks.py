26489166f858e5021d24437de3dad6fd5d69aa39
SKIBIDI-DOP-DOP-DOP-YES-YES <yukari8327@hotmail.com>
2024-12-18 00:14:30 +0000

    jul.it7y6u

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -1,27 +1,50 @@
-"""
-For this program, you will tell Tina the Turtle to draw 
-a triangle.
+import turtle
+import time
+skibiditoilet=turtle.Turtle()
+skibiditoilet.shape('arrow')
+skibiditoilet.speed(0)
+skibiditoilet.hideturtle()
 
-You should look at the previous program, 02_Meet_Tina.py
-to see how to use the turtle commands.
+s=turtle.Screen()
 
+while True:
+    for i in range(40):
+        time.sleep(0.0000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001999999989988998988989898898999888988989889898988989899878889989988889888998898899898899898898989888898989898899898898989000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
+        if i % 2 == 0:
+            s.bgcolor('red')
+        else:
+            s.bgcolor('lime')  
 
-"""
+    #1st circle
+    skibiditoilet.penup()
+    skibiditoilet.goto(0,-200)
+    skibiditoilet.pendown()
+    skibiditoilet.begin_fill()
+    skibiditoilet.color('white')
+    skibiditoilet.circle(80)
+    skibiditoilet.end_fill()
 
-# These lines are needed in most turtle programs
-import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+    #2nd circle
+    skibiditoilet.penup()
+    skibiditoilet.goto(0,-40)
+    skibiditoilet.pendown()
+    skibiditoilet.begin_fill()
+    skibiditoilet.color('white')
+    skibiditoilet.circle(50)
+    skibiditoilet.end_fill()
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+    #3rd head
+    skibiditoilet.penup()
+    skibiditoilet.goto(0,60)
+    skibiditoilet.pendown()
+    skibiditoilet.begin_fill()
+    skibiditoilet.color('white')
+    skibiditoilet.circle(30)
+    skibiditoilet.end_fill()
 
-# Use tina.forward() and tina.left() to draw a triangle
-# Make each side of the triangle a different color with 
-# tina.pencolor()
-
-... # Your code here
-tina.forward(400)
-tina.left(120)
-tina.forward(285)
-tina.left(120)
-tina.forward(300)
-turtle.exitonclick()                    # Close the window when we click on it
+    skibiditoilet.penup()
+    skibiditoilet.goto(0,75)
+    skibiditoilet.pendown()
+    skibiditoilet.begin_fill()
+    skibiditoilet.color('black')
+    skibiditoilet
